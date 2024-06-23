8f543b4967d81250c6ebf5279976d1c8023b6bf8
totato4 <v4.97@list.ru>
2024-06-23 14:53:39 +0300

    postgres connected

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
 import pg from "pg";
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "nemongo",
-//   host: "localhost",
-//   port: 5432,
-//   database: "sasa",
-// });
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new pg.Pool({
   connectionString: process.env.POSTGRES_URL,
 });
+
+console.log(process.env.POSTGRES_URL);
 
 // const Pool = require("pg").Pool;
 // const pool = new Pool({
