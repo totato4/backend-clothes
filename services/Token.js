764234893ef7d38523f8a226a18d6cbff0b834f5
totato4 <v4.97@list.ru>
2024-06-25 06:30:31 +0300

    fix env

--- services/Token.js
+++ services/Token.js
@@ -6,16 +6,18 @@ dotenv.config();
 
 class TokenService {
   static async generateAccessToken(payload) {
-    return await jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    return await jwt.sign(payload, "access_naruto", {
       expiresIn: "30m",
     });
   }
+  // process.env.ACCESS_TOKEN_SECRET
 
   static async generateRefreshToken(payload) {
-    return await jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    return await jwt.sign(payload, "refresh_sasuke", {
       expiresIn: "15d",
     });
   }
+  // process.env.REFRESH_TOKEN_SECRET
 
   static async checkAccess(req, _, next) {
     const authHeader = req.headers.authorization;
