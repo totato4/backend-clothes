9967fcb67982120905fbc92747cb41fae20e8db2
totato4 <v4.97@list.ru>
2024-07-03 07:00:20 +0300

    fix getCart error

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -4,6 +4,6 @@ import CartController from "../controller/cart.controller.js";
 const router = Router();
 
 router.post("/getCart", CartController.getCart);
-router.post("/update", CartController.updateCart);
+router.patch("/update", CartController.updateCart);
 
 export default router;
