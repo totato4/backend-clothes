be2b7120cc31e40f54861a56f68caa5ff0d3dff9
totato4 <v4.97@list.ru>
2025-04-04 13:14:47 +0300

    cookies settings настроены

--- constants.js
+++ constants.js
@@ -1,6 +1,10 @@
 export const COOKIE_SETTINGS = {
   REFRESH_TOKEN: {
     httpOnly: true,
+    secure: true,
+    sameSite: "none", // Или "lax", если фронтенд и бэкенд на одном домене
+    path: "/",
+    // domain: undefined (не указываем!)
     maxAge: 1296e6, // 15 * 24 * 3600 * 1000 (15 дней)
   },
 };
